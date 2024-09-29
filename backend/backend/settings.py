756a1919d8bb5d4f5fa5c3a2b4e31c8a652cc917
Jahiem Allen <justanotherinternetrandom@gmail.com>
2024-09-29 03:11:20 -0400

    vscode git glitch, changed Django database to PostGreSql

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "db_name",
+        'USER': "db_username",
+        'PASSWORD': "db_password",
+        'HOST': "db_host",
+        'PORT': "db_port",  # 5432 by default
     }
 }
 
